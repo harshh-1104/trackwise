0c8c01dea1478aedbbd1f244c6ff5e8d10e70b36
Harshh-1104 <chvharsh@gmail.com>
2026-03-30 18:48:43 +0530

    commit

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-984b(iwf(56avu93+9af&!d%r3#ahkd059u-@+w_76r2s$%e+#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
