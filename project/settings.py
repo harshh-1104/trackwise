dc2b291501de1ea39eaa1672d7f14e42221ea202
Harshh-1104 <chvharsh@gmail.com>
2026-04-01 12:36:45 +0530

    commit

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-984b(iwf(56avu93+9af&!d%r3#ahkd059u-@+w_76r2s$%e+#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
